0b379e6523079b7958bf9200dd90ec35e08b8ad1
Jefferson0k <jefersoncovenas7@gmail.com>
2026-01-12 22:11:12 -0500

    solucion

--- resources/js/pages/panel/RoomDetails/indexRoomDetails.vue
+++ resources/js/pages/panel/RoomDetails/indexRoomDetails.vue
@@ -1,36 +1,6 @@
 <template>
 
-    <Head title="Ocupada" />
-    <AppLayouth>
-        <div>
-            <template v-if="isLoading">
-                <Espera />
-            </template>
-            <template v-else>
-                <div class="card">
-                    <listRommDetails :roomData="roomData" />
-                </div>
-            </template>
-        </div>
-    </AppLayouth>
 </template>
-
 <script setup lang="ts">
-import { ref, onMounted, computed } from 'vue';
-import AppLayouth from '@/layout/AppLayouth.vue';
-import { Head, usePage } from '@inertiajs/vue3';
-import Espera from '@/components/Espera.vue';
-import listRommDetails from './Desarrollo/listRommDetails.vue';
 
-const page = usePage();
-
-const roomData = computed(() => page.props.data?.data || page.props.data);
-
-const isLoading = ref(true);
-
-onMounted(() => {
-    setTimeout(() => {
-        isLoading.value = false;
-    }, 1000);
-});
 </script>
